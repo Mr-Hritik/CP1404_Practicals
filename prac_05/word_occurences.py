eadb705e4669ca686ef77716889a4c69ffc1e312
Mr-Hritik <63349715+Mr-Hritik@users.noreply.github.com>
2020-04-25 23:33:48 +0530

    display number of occurrence of different words text entered

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,14 +1,16 @@
+"""https://github.com/Mr-Hritik/CP1404_Practicals/blob/master/prac_05/word_occurences.py"""
+
 def main():
     count_words = {}
-    text = input("Text: ")
-    words_in_text = text.split()
-    for word in words_in_text:
+    sentence = input("Enter a sentence: ")
+    words_in_sentence = sentence.split()
+    for word in words_in_sentence:
         count = count_words.get(word, 0)
         count_words[word] = count + 1
-    words_in_text = list(count_words.keys())
-    words_in_text.sort()
-    maximum_length = max(len(word) for word in words_in_text)
-    for word in words_in_text:
+    words_in_sentence = list(count_words.keys())
+    words_in_sentence.sort()
+    maximum_length = max(len(word) for word in words_in_sentence)
+    for word in words_in_sentence:
         print("{:{}} : {}".format(word, maximum_length, count_words[word]))
 
 main()
